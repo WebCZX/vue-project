d1827c8ae416757570b350f3d5101685946395f4
czx <1533266877@qq.com>
2023-11-02 16:00:57 +0800

    finish login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -13,7 +13,7 @@
                         <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
                     </el-form-item>
                     <el-form-item>
-                        <el-button class="login_btn" type="primary" size="default" @click="login">
+                        <el-button :loading="loading" class="login_btn" type="primary" size="default" @click="login">
                             登录
                         </el-button>
                     </el-form-item>
@@ -26,15 +26,46 @@
 
 <script setup lang="ts">
 import { User, Lock } from '@element-plus/icons-vue'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import useUserStore from '@/store/modules/user';//引入用户相关的小仓库
+import { useRouter } from 'vue-router';
+import { ElNotification } from 'element-plus';
+//获取路由器
+let $router = useRouter();
+//定义变量控制按钮加载效果
+let loading = ref(false)
+let useStore = useUserStore();
 //收集账号与密码的数据
 let loginForm = reactive({ username: 'admin', password: '111111', })
 //登录按钮回调
-const login = () => {
+const login = async () => {
+    //加载效果：开始加载
+    loading.value = true
     //点击登录按钮后干什么？
     //通知仓库发登录请求
     //请求成功->首页展示数据的地方
     //请求失败->弹出登陆失败信息
+    try {
+        //保证登录成功
+        await useStore.userLogin(loginForm);
+        //编程式导航跳转到展示数据首页
+        $router.push("/");
+        //登陆成功提示信息
+        ElNotification({
+            type: 'success',
+            message: '登录成功'
+        });
+        //登陆成功加载效果也消失
+        loading.value = false;
+    } catch (error) {
+        //登录失败加载效果消息
+        loading.value = false;
+        //登陆失败的提示信息
+        ElNotification({
+            type: 'error',
+            message: (error as Error).message
+        })
+    }
 }
 </script>
 
