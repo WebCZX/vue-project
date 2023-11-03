3e4fa04c58e111d2ea50a91dfc95b9dc4809fa95
czx <1533266877@qq.com>
2023-11-03 17:41:04 +0800

    完成登录自定义校验表单/Complete the login custom verification form

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,13 +3,14 @@
         <el-row>
             <el-col :span="12" :xs="0"></el-col>
             <el-col :span="12" :xs="24">
-                <el-form class="login_form">
+                <!-- 登录的表单 -->
+                <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
                     <h1>你好！</h1>
                     <h2>欢迎来到我的商城</h2>
-                    <el-form-item>
+                    <el-form-item prop="username">
                         <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
                     </el-form-item>
-                    <el-form-item>
+                    <el-form-item prop="password">
                         <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
                     </el-form-item>
                     <el-form-item>
@@ -37,10 +38,15 @@ let $router = useRouter();
 //定义变量控制按钮加载效果
 let loading = ref(false)
 let useStore = useUserStore();
+//获取el-form组件
+let loginForms = ref();
 //收集账号与密码的数据
 let loginForm = reactive({ username: 'admin', password: '111111', })
 //登录按钮回调
 const login = async () => {
+    //保证全部表单相校验通过再发请求
+    await loginForms.value.validate();
+
     //加载效果：开始加载
     loading.value = true
     //点击登录按钮后干什么？
@@ -69,6 +75,54 @@ const login = async () => {
             message: (error as Error).message
         })
     }
+}
+
+//自定义校验规则函数
+const valifatorUserName = (_rule: any, value: any, callback: any) => {
+    //rule：校验规则对象
+    //value：表单元素文本内容
+    //函数：如果符合条件callback放行通过即为
+    //如果不符合条件callback方法，注入错误提示信息
+    if (value.length <= 5) {
+        callback(new Error('账号长度至少5位'));
+    } else if (value.length >= 10) {
+        callback(new Error('账号长度最多10位'));
+    } else {
+        callback();
+    }
+}
+const valifatorPassword = (_rule: any, value: any, callback: any) => {
+    //rule：校验规则对象,没有引用时前面加“_”可省略报错信息
+    //value：表单元素文本内容
+    //函数：如果符合条件callback放行通过即为
+    //如果不符合条件callback方法，注入错误提示信息
+    if (value.length <= 6) {
+        callback(new Error('密码长度至少6位'));
+    } else if (value.length >= 10) {
+        callback(new Error('密码长度最多10位'));
+    } else {
+        callback();
+    }
+}
+
+//定义表单校验需要配置对象
+const rules = {
+    // 规则对象属性
+    // required：代表这个字段务必要校验
+    // min：文本长度至少多少位
+    // max：文本长度最多多少位
+    // message：错误的提示信息
+    // trigger：触发校验表单的时机 change->文本发生变化出发校验，blur->失去焦点时触发校验
+    username: [
+        // { required: true, min: 6, message: '账号长度至少6位', trigger: 'change' },
+        // { required: true, max: 10, message: '账号长度最多10位', trigger: 'change' }
+        { trigger: 'change', validator: valifatorUserName }
+    ],
+    password: [
+        // { required: true, min: 6, message: '密码长度至少6位', trigger: 'change' },
+        // { required: true, max: 10, message: '账密码长度最多10位', trigger: 'change' }
+        { trigger: 'change', validator: valifatorPassword }
+    ],
 }
 </script>
 
