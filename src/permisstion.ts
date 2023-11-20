99f1d29d6bcdb917f59b995e35a1baf123e23dff
czx <1533266877@qq.com>
2023-11-20 13:51:53 +0800

    鉴权路由完成一半

--- src/permisstion.ts
+++ src/permisstion.ts
@@ -1,9 +1,17 @@
+import { path } from 'path';
 // import { nprogress } from '@/nprogress';
 //路由鉴权：项目当中路由能不能被访问的权限设置(某一个路由什么条件下能被访问，什么条件下不能被访问)
 import router from '@/router';
 import nprogress from 'nprogress';
 //引入进度条样式
 import 'nprogress/nprogress.css';
+
+//获取用户相关的小仓库内部token数据，判断用户是否登陆成功
+import useUserStore from './store/modules/user';
+import pinia from './store';
+let userStore = useUserStore(pinia);
+console.log(userStore);
+
 //全局守卫：项目当中任意路由的切换都会触发的钩子
 //全局前置守卫
 router.beforeEach((to: any, from: any, next: any) => {
@@ -12,6 +20,25 @@ router.beforeEach((to: any, from: any, next: any) => {
 	//next：路由放行函数
 	//访问某一个路由之前守卫
 	nprogress.start();
+	//获取token，判断用户有没有登录
+	let token = userStore.token;
+	//用户登陆判断
+	if (token) {
+		//登陆成功，访问login。不能访问。指向首页
+		if (to.path == '/login') {
+			next({ path: '/' });
+		} else {
+			//登陆成功访问其余6个路由(登录排除)
+			next();
+		}
+	} else {
+		//用户未登录判断
+		if (to.path == '/login') {
+			next();
+		} else {
+			next({ path: '/login', query: { redirect: to.path } });
+		}
+	}
 	next();
 });
 
@@ -21,3 +48,8 @@ router.afterEach((to: any, from: any) => {
 });
 
 //1. 任意路由切换实现进度条业务  ---nprogress
+//2.路由鉴权(路由组件访问权限的设置)
+//全部路由组件：登录|404|任意路由|首页|数据大屏|权限管理(三个子路由)|商品管理(四个子路由)
+
+//用户未登录可以访问login，其余6个路由不能访问(指向login)
+//用户登录成功：不可以访问login[指向首页]，其余路由可以访问
