f0cdfaccac42b47a202ff1e574da114bca651d63
czx <1533266877@qq.com>
2023-11-24 21:53:26 +0800

    完成品牌管理模块静态的搭建

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { createApp } from "vue";
-import App from "@/App.vue";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-import "@/styles/index.scss"; //引入模板的全局的样式
-import "virtual:svg-icons-register"; //引入svg插件需要配置代码
-import globalComponents from "@/components"; //引入自定义插件对象：注册整个项目的全局组件
-import router from "./router"; //引入路由
-import pinia from "./store"; //引入仓库
+import { createApp } from 'vue';
+import App from '@/App.vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import '@/styles/index.scss'; //引入模板的全局的样式
+import 'virtual:svg-icons-register'; //引入svg插件需要配置代码
+import globalComponents from '@/components'; //引入自定义插件对象：注册整个项目的全局组件
+import router from './router'; //引入路由
+import pinia from './store'; //引入仓库
 const app = createApp(App);
-app.use(ElementPlus);
+app.use(ElementPlus, {
+	locale: zhCn,
+});
 
 //安装自定义插件
 app.use(globalComponents);
@@ -19,8 +21,8 @@ app.use(router);
 //安装仓库
 app.use(pinia);
 
-app.mount("#app");
+app.mount('#app');
 
 //引入路由鉴权文件
-import "./permisstion";
-
+import './permisstion';
+import { zhCn } from 'element-plus/es/locales.mjs';
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,11 +1,43 @@
 <template>
-    <div>
-        <h1>品牌管理</h1>
-    </div>
+    <el-card class="box-card">
+        <!-- 卡片顶部添加按钮 -->
+        <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
+        <!-- 表格组件：用于展示已有的平台数据 -->
+        <!-- 
+            table
+            ---border：设置表盒纵向是否有边框
+            tanle-column
+            ---label：某一个列表
+            ---width：设置这列宽度
+            ---align：设置这一列对齐方式
+         -->
+        <el-table style="margin:10px 0 ;" border>
+            <el-table-column label="序号" width="80px" align="center"></el-table-column>
+            <el-table-column label="品牌名称"></el-table-column>
+            <el-table-column label="品牌LOGO"></el-table-column>
+            <el-table-column label="品牌操作"></el-table-column>
+        </el-table>
+        <!-- 分页器组件
+            pagination
+                v-model:current-page:设置分页器当前页码
+                v-model:page-size:设置每一个展示数据条数
+                page-sizes:用于设置下拉菜单数据
+                background:设置分页器按钮背景颜色
+                laout:设置分页器6个子组件布局调整
+        -->
+        <el-pagination v-model:current-page="pageNo" v-model="limit" :page-sizes="[3, 5, 7, 9]" :background="true"
+            layout="prev,pager,next,jumper,->,sizes,total" :total="400" />
+    </el-card>
 </template>
 
 <script setup lang="ts">
 
+//引入组合式API函数ref
+import { ref } from 'vue';
+//当前页码
+let pageNo = ref<number>(1)
+//定义每一页展示多少条数据
+let limit = ref<number>(3)
 </script>
 
 <style scoped></style>
