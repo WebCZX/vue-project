9b431948b3b3d68b9d0a72b700ffcdbd232c8e26
czx <1533266877@qq.com>
2023-11-25 20:28:40 +0800

    完成对话框dialog静态搭建

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,9 +1,10 @@
 <template>
-    <el-card class="box-card">
-        <!-- 卡片顶部添加按钮 -->
-        <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
-        <!-- 表格组件：用于展示已有的平台数据 -->
-        <!-- 
+    <div>
+        <el-card class="box-card">
+            <!-- 卡片顶部添加按钮 -->
+            <el-button type="primary" size="default" icon="Plus" @click="addTrademark">添加品牌</el-button>
+            <!-- 表格组件：用于展示已有的平台数据 -->
+            <!-- 
             table
             ---border：设置表盒纵向是否有边框
             tanle-column
@@ -11,27 +12,27 @@
             ---width：设置这列宽度
             ---align：设置这一列对齐方式
          -->
-        <el-table style="margin:10px 0 ;" border :data="trademarkArr">
-            <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
-            <!-- table-column:默认展示数据用div -->
-            <el-table-column label="品牌名称">
-                <template #="{ row, $index }">
-                    <pre style="color: brown;">{{ row.tmName }}</pre>
-                </template>
-            </el-table-column>
-            <el-table-column label="品牌LOGO">
-                <template #="{ row, $index }">
-                    <img :src="row.logoUrl" alt="暂无图片" style=" width: 100px;height: 100px;">
-                </template>
-            </el-table-column>
-            <el-table-column label="品牌操作">
-                <template #="{ row, $index }">
-                    <el-button type="success" size="small" icon="Edit"></el-button>
-                    <el-button type="danger" size="small" icon="Delete"></el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-        <!-- 分页器组件
+            <el-table style="margin:10px 0 ;" border :data="trademarkArr">
+                <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
+                <!-- table-column:默认展示数据用div -->
+                <el-table-column label="品牌名称">
+                    <template #="{ row, $index }">
+                        <pre style="color: brown;">{{ row.tmName }}</pre>
+                    </template>
+                </el-table-column>
+                <el-table-column label="品牌LOGO">
+                    <template #="{ row, $index }">
+                        <img :src="row.logoUrl" alt="暂无图片" style=" width: 100px;height: 100px;">
+                    </template>
+                </el-table-column>
+                <el-table-column label="品牌操作">
+                    <template #="{ row, $index }">
+                        <el-button type="success" size="small" icon="Edit" @click="updateTrademark"></el-button>
+                        <el-button type="danger" size="small" icon="Delete" @click=""></el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <!-- 分页器组件
             pagination
                 v-model:current-page:设置分页器当前页码
                 v-model:page-size:设置每一个展示数据条数
@@ -39,10 +40,34 @@
                 background:设置分页器按钮背景颜色
                 laout:设置分页器六个子组件布局调整
         -->
-        <el-pagination @size-change="sizeChange" @current-change="getHasTrademark" paper-count="9"
-            v-model:current-page="pageNo" v-model:page-size="limit" :page-sizes="[3, 5, 7, 9]" :background="true"
-            layout="prev,pager,next,jumper,->,sizes,total" :total="400" />
-    </el-card>
+            <el-pagination @size-change="sizeChange" @current-change="getHasTrademark" paper-count="9"
+                v-model:current-page="pageNo" v-model:page-size="limit" :page-sizes="[3, 5, 7, 9]" :background="true"
+                layout="prev,pager,next,jumper,->,sizes,total" :total="400" />
+        </el-card>
+        <!-- 对话框组件：在添加品牌与修改已有品牌的业务时候使用结构 -->
+        <!-- v-model:控制对话框显示与隐藏：true显示，false隐藏 -->
+        <el-dialog v-model="dialogFormVisible" title="添加品牌">
+            <el-form style="width: 80%;" label-width="80px">
+                <el-form-item label="品牌名称">
+                    <el-input placeholder="请输入品牌名称"></el-input>
+                </el-form-item>
+                <el-form-item label="品牌LOGO">
+                    <el-upload class="avatar-uploader" action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                        :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                        <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+                        <el-icon v-else class="avatar-uploader-icon">
+                            <Plus />
+                        </el-icon>
+                    </el-upload>
+                </el-form-item>
+            </el-form>
+            <!-- 具名插槽：footer -->
+            <template #footer>
+                <el-button type="danger" size="default" @click="cancel">取消</el-button>
+                <el-button type="success" size="default" @click="confirm">确定</el-button>
+            </template>
+        </el-dialog>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -59,6 +84,8 @@ let limit = ref<number>(3);
 let total = ref<number>(0)
 //存储已有品牌的数据
 let trademarkArr = ref<Records>([]);
+//控制对话框显示与隐藏
+let dialogFormVisible = ref<boolean>(false)
 //获取已有品牌的接口封装为一个函数：在任何环境下向获取数据，调用函数即可
 const getHasTrademark = async (pager = 1) => {
     //当前页码
@@ -74,12 +101,6 @@ const getHasTrademark = async (pager = 1) => {
 onMounted(() => {
     getHasTrademark()
 })
-//分页器当前页码发生变化时触发
-//对于当前页码发生变化自定义事件，组件pagination父组件回传了数据
-// const changePageNo = () => {
-//     //当前页码发生变化时再次发请求获取对应已有品牌数据（当前页码）
-//     getHasTrademark();
-// }
 
 //当下拉菜单发生变化时触发此方法
 //这个自定义事件，分页器组件会将下拉菜单选中数据返回
@@ -89,6 +110,57 @@ const sizeChange = () => {
     getHasTrademark();
 }
 
+//添加品牌按钮回调
+const addTrademark = () => {
+    //对话框显示
+    dialogFormVisible.value = true;
+}
+//修改已有品牌的按钮回调
+const updateTrademark = () => {
+    //对话框显示
+    dialogFormVisible.value = true;
+}
+//对话框底部取消按钮
+const cancel = () => {
+    //对话框隐藏
+    dialogFormVisible.value = false;
+}
+
+const confirm = () => {
+    dialogFormVisible.value = false;
+}
+
 </script>
 
-<style scoped></style>
+
+
+<style scoped>
+.avatar-uploader .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+    border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    text-align: center;
+}
+</style>
